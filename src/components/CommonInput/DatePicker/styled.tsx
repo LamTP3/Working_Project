bd9682bbb34fd82f8823929d7b628b3228a3bde1
LamTP3 <phuclamtran2810@gmail.com>
2024-07-26 10:56:42 +0700

    demo 2 26/7

--- src/components/CommonInput/DatePicker/styled.tsx
+++ src/components/CommonInput/DatePicker/styled.tsx
@@ -12,6 +12,16 @@ export const DatePickerWarraper = styled.div<Props>`
     width: ${(props) => (props.$width ? props.$width : "80%")};
 
     opacity: ${(props) => (props.$disabled ? "0.3" : "1")};
+    .ant-picker-clear {
+      left: calc(100% - 45px);
+      width: 14px;
+      opacity: 1;
+    }
+    &:hover {
+      .ant-picker-suffix {
+        opacity: 1;
+      }
+    }
   }
   .ant-picker-outlined {
     background-color: var(--primary-input-bg) !important;
