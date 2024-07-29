f1acc304a3e1e5d6b0164b35c065ea991113da4a
LamTP3 <phuclamtran2810@gmail.com>
2024-07-29 15:57:46 +0700

    demo 1 29/7

--- src/components/CommonInput/DatePicker/DatePicker.tsx
+++ src/components/CommonInput/DatePicker/DatePicker.tsx
@@ -1,56 +1,26 @@
 import React from "react";
-import { DatePicker } from "antd";
-import type { DatePickerProps } from "antd";
+import { DatePicker, DatePickerProps } from "antd";
 import { DatePickerWarraper } from "./styled";
-import { Props } from "./DatePickerType";
 import { DateIcon } from "../../../Icon";
-import dayjs, { Dayjs } from "dayjs";
 import { CloseOutlined } from "@ant-design/icons";
 import { dateFormat } from "../../../helper/util";
-// Định dạng ngày giờ
+import dayjs from "dayjs";
 
-const DatePickerComponent: React.FC<Props> = ({
-  disabled,
-  width,
-  placeholder,
-  value,
-  minDate,
-  onChange,
-}) => {
-  const onChangeProp: DatePickerProps["onChange"] = (date, dateStr) => {
-    if (onChange) {
-      onChange(date, dateStr);
-    }
-    console.log("Date String:", dateStr);
-    console.log("Begin date:", date);
-    console.log(
-      "Formatted to exact date we need:",
-      date ? date.format(dateFormat) : "No date"
-    );
-  };
-
-  const dayjsValue = value ? dayjs(value, dateFormat) : undefined;
-
-  const disabledDate = (current: Dayjs) => {
-    if (minDate) {
-      const today = dayjs().startOf("day");
-      return current && current.isBefore(today);
-    }
-    return false;
-  };
+const DatePickerComponent: React.FC<DatePickerProps> = (props) => {
+  const { disabled, placeholder, value, onChange, onBlur, ...rest } = props;
 
   return (
-    <DatePickerWarraper $width={width} $disabled={disabled}>
+    <DatePickerWarraper $disabled={disabled}>
       <DatePicker
         showTime
-        onChange={onChangeProp}
         placeholder={placeholder || "MM/dd/yyyy HH:mm"}
         suffixIcon={<DateIcon />}
         format={dateFormat}
-        disabledDate={disabledDate}
-        disabled={disabled}
-        value={dayjsValue}
+        value={value ? dayjs(value) : null}
+        onBlur={onBlur}
+        onChange={onChange}
         allowClear={{ clearIcon: <CloseOutlined /> }}
+        {...rest}
       />
     </DatePickerWarraper>
   );
