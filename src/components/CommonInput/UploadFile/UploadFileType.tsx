21be80767586bd2cab365601a71e85198a6dc623
LamTP3 <phuclamtran2810@gmail.com>
2024-08-06 09:44:17 +0700

    6/8

--- src/components/CommonInput/UploadFile/UploadFileType.tsx
+++ src/components/CommonInput/UploadFile/UploadFileType.tsx
@@ -1,6 +1,7 @@
 import type { UploadProps as AntdUploadProps } from "antd";
 
 export interface UploadFileProps extends AntdUploadProps {
+  value?: string;
   label: string;
   height?: string;
   width?: string;
